7c2dc71fa3097edc22da04682990ecded26968e7
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-04-10 03:58:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,7 +20,29 @@ class Artist
     @@all << self
   end
 
-  def find_or_create_by_name(name)
+  def self.all
+    @@all
+  end
 
+  def find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+  def self.find(name)
+    self.all.detect { |artist| artist.name == name }
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+  def print_songs
+    self.songs.each {|song| song.name}
   end
 end
