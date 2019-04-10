7ce29dfbfe76d84cd6233101a23f4f3ad1dd1470
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-04-10 03:47:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,17 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
   def add_song(name)
     song = Song.new(name)
+    @songs << song
+  end
+
+  def songs
+    @songs
   end
 end
